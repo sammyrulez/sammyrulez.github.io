7757b9d7818a324c8f2eef9a5d481f4b7d76d1c8
Sam <sam@dev.com>
2024-05-24 14:57:27 +0200

    new init

---
+++ pelicanconf.py
@@ -0,0 +1,43 @@
+AUTHOR = 'Sam Reghenzi'
+SITENAME = 'Sam Reghenzi Homepage'
+SITEURL = ""
+
+PATH = "content"
+
+TIMEZONE = 'Europe/Rome'
+
+DEFAULT_LANG = 'en'
+THEME = 'medius'
+
+MEDIUS_CATEGORIES = {
+    'Review': {
+        'description': 'A galaxy is a gravitationally bound system of stars, stellar remnants, interstellar gas and dust, and dark matter.',
+        'logo': 'https://upload.wikimedia.org/wikipedia/commons/thumb/b/b9/GalacticRotation2.svg/250px-GalacticRotation2.svg.png',
+        'thumbnail': 'https://upload.wikimedia.org/wikipedia/commons/thumb/2/29/NGC_3923_Elliptical_Shell_Galaxy.jpg/220px-NGC_3923_Elliptical_Shell_Galaxy.jpg'
+    }
+}
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+# Blogroll
+LINKS = (
+    ("Pelican", "https://getpelican.com/"),
+    ("Python.org", "https://www.python.org/"),
+    ("Jinja2", "https://palletsprojects.com/p/jinja/"),
+    ("You can modify those links in your config file", "#"),
+)
+
+# Social widget
+SOCIAL = (
+    ("You can add links in your config file", "#"),
+    ("Another social link", "#"),
+)
+
+DEFAULT_PAGINATION = False
+
+# Uncomment following line if you want document-relative URLs when developing
+# RELATIVE_URLS = True
