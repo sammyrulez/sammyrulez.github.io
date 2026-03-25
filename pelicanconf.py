bda4d84a9007ceb5046a3d375dbdc48edc612c47
Sam <sam@dev.com>
2026-03-25 10:50:03 +0100

    clean up

--- pelicanconf.py
+++ pelicanconf.py
@@ -11,7 +11,8 @@ THEME = 'theme/chunk'
 
 PLUGINS = ['pelican.plugins.yaml_metadata']
 # Feed generation is usually not desired when developing
-FEED_ALL_ATOM = None
+FEED_ALL_ATOM = 'feeds/all.atom.xml'
+FEED_ALL_RSS = 'feeds/all.rss.xml'
 CATEGORY_FEED_ATOM = None
 TRANSLATION_FEED_ATOM = None
 AUTHOR_FEED_ATOM = None
